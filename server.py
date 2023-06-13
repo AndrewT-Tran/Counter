51d1c4e88809ef6dfd5aaaf15348e17a35ca3f90
Andrew T <me@andrewtran-shi.dev>
2023-06-12 17:42:29 -0700

    need to fix reset

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, session, redirect, url_for, render_template
+
+app = Flask(__name__)
+app.secret_key = "your_secret_key_here"
+
+
+@app.route("/")
+def index():
+    if "count" not in session:
+        session["count"] = 0
+    return render_template("index.html", count=session["count"])
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    if "count" not in session:
+        session["count"] = 0
+    session["count"] += 1
+    return redirect(url_for("index"))
+
+
+@app.route("/decrease", methods=["POST"])
+def decrease():
+    if "count" not in session:
+        session["count"] = 0
+    session["count"] -= 1
+    return redirect(url_for("index"))
+
+
+@app.route("/destroy_session")
+def reset():
+    session.clear()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
